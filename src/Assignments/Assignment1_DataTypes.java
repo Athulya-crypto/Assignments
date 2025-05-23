22efdd7f9862358e369b038f18e602c84c441751
Athulya-crypto <athulyaremesh92@gmail.com>
2025-05-23 08:13:20 +0530

    Assignments

---
+++ src/Assignments/Assignment1_DataTypes.java
@@ -0,0 +1,99 @@
+package Assignments;
+
+public class Assignment1_DataTypes {
+
+	public static void main(String[] args) {
+		
+ //	1.Temperature of a city in degrees Celsius: 25.5
+		float Temperature=25.5f;
+		System.out.println(Temperature);
+
+// 2.Whether a customer has placed an order: true or false
+ boolean Order=true;
+ System.out.println(Order);
+ 
+// 3.Person's phone number: "123-456-7890"
+
+String phoneNumber="123-456-7890";
+System.out.println(phoneNumber);
+
+//4. Amount of money in a customer's bank account: 1000.50
+
+float bankBalance=1000.50f;
+System.out.println(bankBalance);
+
+// 5.Person's email address: "john.doe@example.com"
+String emailId="john.doe@example.com";
+System.out.println(emailId);
+
+//6.Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+double latitude=37.7749;
+double longitude=-122.4194;
+System.out.println(latitude+ ","+longitude);
+
+//7. Person's marital status: true or false
+   // if the person is married
+boolean married=true;
+ System.out.println(married);
+ 
+      // if the person is unmarried
+         boolean unmarried=false;
+         System.out.println(unmarried);
+ 
+// 8. Person's occupation: "Software Engineer"
+ String occupation= "Software Engineer";
+ System.out.println(occupation);
+ 
+// 9. Person's favourite colour: "Blue"
+
+ String favouriteColour="Blue";
+ System.out.println(favouriteColour);
+ 
+ // 10.Current year: 2023
+ 
+short currentYear= 2023;
+System.out.println(currentYear);
+
+//11.Number of followers on a social media platform: 1,000,000
+long numberOfFollowers=1000000;
+System.out.println(numberOfFollowers);
+
+//12.Rating of a movie: 7.5
+float rating=7.5f;
+System.out.println(rating);
+
+//13.Person's blood type: 'A'
+char bloodType='A';
+System.out.println(bloodType);
+
+//14.Title of a book: "To Kill a Mockingbird"
+String bookTitle="To Kill a Mockingbird";
+System.out.println(bookTitle);
+
+//15.Number of employees in a company: 500
+int employeeCount=500;
+System.out.println(employeeCount);
+
+//16.Time of an event: 2:30 PM
+String TimeOfEvent="2:30 PM ";
+System.out.println(TimeOfEvent);
+
+//17.Name of a country: "United States"
+String nameOfCountry="United States";
+System.out.println(nameOfCountry);
+
+//18.Person's eye color: "Brown"
+String personEyeColor="Brown";
+System.out.println(personEyeColor);
+
+//19.Person's birthplace: "New York City"
+String personBirthCity="New York City";
+System.out.println(personBirthCity);
+
+//20.Distance between two cities: 200.5
+float distance=200.5f;
+System.out.println(distance);
+
+	}
+
+}
