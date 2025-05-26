3e09c8a46952091079d70842b5fa483191eb6826
Athulya-crypto <athulyaremesh92@gmail.com>
2025-05-26 12:51:51 +0530

    Assignment 2 completed

--- src/Assignments/Assignment2_Array.java
+++ src/Assignments/Assignment2_Array.java
@@ -4,118 +4,42 @@ public class Assignment2_Array {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String [] semesters= {"Sem 1", "Sem 2","Sem 3","Sem 4","Sem 5"};
-
-String [] subjects= {"Subject1","Subject2","Subject3","Subject4","Subject5"};
-String [] status= {"Stats1","Stats2","Stats3","Stats4","Stats5"};
 
 
-String[][]semesterSubjects= {semesters,subjects};
-String [][]subjectsStatus= {subjects,status};
-
-String [] subj1= {"Maths2","Mechanics","Environmental science","Basic electronics","Engineering physics","Engineering graphics"};
-String[] status1= {"Pass","Pass","Pass","Fail","fail","Pass"};
-
-String[][]subj1Status1= {subj1,status1};
-
-String[][][]semSubStatus1= {semesterSubjects,subj1Status1};
-
-System.out.println(semSubStatus1[0][0][1] +"");
-
-//String[][] semester2SubStatus= {semester2Subj,semseter2Status};
-
-//creating a 3-D array
-//String[][][]
-//
-//
-//System.out.println(semester2SubStatus[0][3]);
-//System.out.println(semester2SubStatus[0][4]);
-
-//
-//		String [] sem1Sub= {,,, ,,};
-//		String [] sem2= {,,,,,};
-//		String [] sem3= {,,,,,};
-//		String[] sem4= {,,,,,};
-//		String [] sem5= {,,,,,};
-//
-//		
-//		
-//		
-//		
-//		String [] sem1Status= {, ",,"Pass(74)","Pass(88)","Pass(79)"};	
-//		String [] sem2Status= {,,,"Fail(19)","Fail(24)","Pass(90)"};
-//		String [] sem3Status= {,,,"Fail(32)","Pass(85)","Pass(78)"};	
-//		String [] sem4Status= {,,,"Pass(80)","Pass(76)","Pass(87)"};		
-//		String [] sem5Status= {,,,"Pass(95)","Pass(73)","Pass(90)"};
-
-		
-		
-//		
-//		String[][] semSubStatus= {};
-		
+String [][][] data= {
+		{//semester-1
 			
+			 {"Mathematics I","Physics","Chemistry","Computer Programming","Engineering Drawing","Basic Electrical Eng."},
+				 {"Pass(78)","Pass(85)","Fail(21)","Pass(74)","Pass(88)", "Pass(79)"}},
 		
-//		
-//		
-//		String[][] sem1SubStatus= {sem1Sub,sem1Status};
-//		
-//		
-//		
-//		
-//		
-//		
-//		String [] subjects= {"Subject1","Subject2","Subject3","Subject4","Subject5","Subject6"};
-//		String[][] semSubjects= {sem,subjects};
-//		
-//		
-//		
-//		//String [][] sem1Status= {sem1,status};
-//		System.out.println(sem1Status[1][1]);
-		
-//		String [] sem= {"Sem1","Sem2","Sem3","Sem4","Sem5"}; // 1D
-//		 String [] subjects= {"subject1","subject2","subject3","subject4","subject5","subject6"};
-//		String [] status= {"status1","status 2","status3","status4","status 5"};
-//		 String [][] subStatus= {subjects,status}; //2D
-//		 
-//	String[][]sem1SubNameStatus= {{"Mathematics I", "Pass(78)"},{"Physics", "Pass(85)"},{"Chemistry", "Fail(21)"},{"Computer Programming", "Pass(74)"},
-//            {"Engineering Drawing", "Pass(88)"},{"Basic Electrical Eng.", "Pass(79)"}};
-//		 
-//	String[][]sem2SubNameStatus= {{"Mathematics II", "Pass(82)"},{"Mechanics", "Pass(77)"},{"Environmental Sci.", "Pass(93)"},{"Basic Electronics", "Fail(19)"},
-//            {"Engineering Physics", "Pass(90)"},{"Engineering Graphics", "Pass(87)"}};
-//	String[][]sem3SubNameStatus=      {{"Data Structures", "Pass(88)"},{"Discrete Mathematics", "Pass(81)"},{"Digital Electronics", "Pass(76)"},
-//            {"Operating Systems", "Pass(92)"}, {"Signals and Systems", "Pass(85)"},{"Object-Oriented Prog.", "Pass(78)"}};
-//	String[][]sem4SubNameStatus= { {"Algorithms", "Pass(91)"},{"Computer Networks", "Pass(73)"},{"Database Systems", "Fail(19)"},
-//            {"Microprocessors", "Pass(88)"},{"Communication Eng.", "Pass(76)"},{"Software Engineering", "Pass(90)"}  };
-//	String[][]sem5SubNameStatus=
-//	String[][]sem6SubNameStatus=
-									
-		 
-		 
-		 
-		
-		 
-		 
-//		 String [] subject1= {"Mathematics 1","Mathematics II","Data Structures","Algorithms","Prabability and status"};
-//		 String [] subject2= {"Physics","Mechanics ","Discrete Mathematics","Computer Networks","Machine Learning"};
-//		 String [] subject3= {"Chemistry","Environmental Science","Digital Electronics","Database Systems","Compiler Design"};
-//		 String [] subject4= {"Computer Programming","Basic Electronics","Operating Systems","Microprocessors","Theory of Computation"};	
-//		 String [] subject5= {"Engineering Drawing","Engineering Physics","Signal and Systems","Communication Engineering","Embedded Systems"};
-//		 String [] subject6= {"Basic electrical Eng","Engineering Graphics","Object Oriented prgmg","Software Engineering","Computer Graphics"};
-//		 
-//		 
-//		 String []status1= {"Pass(78)","Pass(82)","Pass(88)","Pass(91)","Pass(86)"};
-//		 String []status2= {"Pass(85)","Pass(77)","Pass(81)","Pass(73)","Pass(88)"};
-//		 String []status3= {"Fail(21)","Pass(93)","Pass(76)","Fail(19)","Pass(84)"};
-//		 String []status4= {"Pass(74)","Fail(19)","Fail(32)","Pass(80)","Pass(95)"};
-//		 String []status5= {"Pass(88)","Fail(24)","Pass(85)","Pass(76)","Pass(73)"};
-//		 String []status6= {"Pass(79)","Pass(90)","Pass(78)","Pass(87)","Pass(87)"};
-		 
-		 
-		 
-		 
+		{// semester 2
+			{"Maths2","Mechanics","Environmental science","Basic electronics","Engineering physics","Engineering graphics"},
+			{"Pass(82)","Pass(77)","Pass(93)","Fail(19)","Pass(90)","Pass(87)"}},
 
-
+		{//Semester-3
+			{"Data Structure","Discrete Mathematics","Digital Electronics","Operating Systems","Signals and Systems","Object Oriented Programming"},
+				{"Pass(88)","Pass(81)","Pass(76)","Fail(32)","Pass(85)","Pass(78)"}
+			},
+		
+		{  //semester -4
+				{"Algorithms","Computer Networks","Database Systems","Microprocessors","Communication Engineering","Software Engineering"},
+				{"Pass(91)","Pass(71)","Fail(19)","Pass(80)","Pass(76)","Pass(87)"}
+		},
+		{//Semester-5
+			{"Probability & Status","Machine Learning","Compiler Design","Theory of computation","Embedded systems","Computer Graphics"},
+			{"Pass(86)","Pass(88)","Pass(84)","Pass(95)","Pass(73)","Pass(90)"}
 		}
 		
+					
+		};
+
+//Print Semester 2 Subject 4 and Subject 5 names.	
+System.out.println("Semester 2 Subject 4 name is " + data[1][0][3]);
+System.out.println("Semester 2 Subject 5 name is " + data[1][0][4]);
 		
-	}
+	//Print the Status/Marks of Semester 4 Subject 3 and Subject 6.	
+System.out.println("The Status/Marks of Semester 4 Subject 3 is "+ data[3][1][2]);
+System.out.println("he Status/Marks of Semester 4 Subject 6 is "+data[3][1][5]);
+
+}
+}
