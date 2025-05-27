018d64bad6a9c3db4f1f4c35cf8038cd249b28b2
Athulya-crypto <athulyaremesh92@gmail.com>
2025-05-27 09:32:05 +0530

    Assignment3 completed

---
+++ src/Assignments/Assignment3_EmpGroups.java
@@ -0,0 +1,18 @@
+package Assignments;
+
+public class Assignment3_EmpGroups {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//Creating an object for the class "Employees" for printing the values			
+		Assignment3_Employees emp=new Assignment3_Employees();
+		
+		System.out.println("EmployeeName: "+ emp.employeeNameAndIDs[0][0] +", "+"Employee ID: "+emp.employeeNameAndIDs[0][1]);
+		System.out.println("EmployeeName: "+ emp.employeeNameAndIDs[1][0]  +", "+"Employee ID: "+emp.employeeNameAndIDs[1][1] );
+		System.out.println("EmployeeName: "+ emp.employeeNameAndIDs[2][0]  +", "+"Employee ID: "+emp.employeeNameAndIDs[2][1] );
+
+
+	}
+
+}
---
+++ src/Assignments/Assignment3_Employees.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+public class Assignment3_Employees {
+
+	
+//Creating 1D array for storing the values for Employee Name and IDs
+
+		String [] employeeNames= {"Bharath","ABC","XYZ"};	
+		String [] employeeIDs= {"1234","1235","1236"};
+
+//Creating 2D array for storing the values for Employee Name and IDs together
+
+		String[][] employeeNameAndIDs= {{"Bharath","1234"},{"ABC","1235"},{"XYZ","1236"}};
+			
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
