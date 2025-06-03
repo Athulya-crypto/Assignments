d89fa3541e3ee93ed81e6ad31e4c439d9e1f7af4
Athulya-crypto <athulyaremesh92@gmail.com>
2025-06-03 08:36:03 +0530

    Assignment 8 Completed

---
+++ src/loopsAndIteratorPrograms/Assignment8_loops.java
@@ -0,0 +1,54 @@
+package loopsAndIteratorPrograms;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Assignment8_loops {
+	int value;
+
+	public static void main(String[] args) {
+
+		int creditCount = 0;
+		int debitCount = 0;
+		int totalCredit=0;
+		int totalDedit=0;
+		int  suspiciousTransactions=0;
+		
+		
+
+		int[] transactions = { 50000, -2000, 3000, -15000, -200, -300, 4000, -3000 };
+
+		for (int value : transactions) {
+			if (value > 0) {
+				
+				creditCount++;
+				totalCredit+=value;
+				;
+			} else {
+				debitCount++;
+				totalDedit+=value;
+				
+
+			}
+			if(Math.abs(value)>10000)
+			{
+				System.out.println("Suspicious credit/ debit Transaction with Amount");
+				suspiciousTransactions+=1;
+			}
+			
+		}
+		int totalAmountInBank=totalCredit+totalDedit;
+		System.out.println("Total number of credit transactions completed" + creditCount);
+		System.out.println("Total number of debit transactions completed" + debitCount);
+		System.out.println("Total credited amount: " + totalCredit);
+		System.out.println("Total debited amount: " + totalDedit);
+		System.out.println("Total amount remaining at bank: " +totalAmountInBank );
+		System.out.println("Total number of suspicious transactions: " +suspiciousTransactions );
+		
+		
+	}
+}
