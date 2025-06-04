eaadd805812074238e971a427b24dd54e4830be1
Athulya-crypto <athulyaremesh92@gmail.com>
2025-06-04 21:01:24 +0530

    Assignment 11 completed

--- src/loopsAndIteratorPrograms/Assignment11_loops.java
+++ src/loopsAndIteratorPrograms/Assignment11_loops.java
@@ -3,24 +3,42 @@ package loopsAndIteratorPrograms;
 public class Assignment11_loops {
 
 	public static void main(String[] args) {
+	int n=5;
+		// outer loop to handle upper part
 		
-		for(int i=1;i<=5;i++)
+		for(int i=1;i<=n;i++)
 		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			
 			for(int j=1;j<=i;j++)
 			{
-				System.out.print(j);
+				System.out.print(j+" ");
 			}
-		System.out.println();
-		}
-		for(int i=5;i>=1;i--)
-		{
-			for(int j=1;j<i;j++)
-			{
-				System.out.print(j);
-			}
-		System.out.println();
-		}
 			
+			
+		System.out.println();
+		}
+		
+		// outer loop to handle upper part	
+		for(int i=4;i>=1;i--)
+		
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j+" ");
+			}
+			
+			
+		System.out.println();
+		}
 
 	}
 
