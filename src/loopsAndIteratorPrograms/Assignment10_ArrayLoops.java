4a541cae969522a8db6dd43b5b4e06ccd7d1986e
Athulya-crypto <athulyaremesh92@gmail.com>
2025-06-04 08:44:31 +0530

    Assignment 9,10,11 completed

--- src/loopsAndIteratorPrograms/Assignment10_ArrayLoops.java
+++ src/loopsAndIteratorPrograms/Assignment10_ArrayLoops.java
@@ -6,21 +6,18 @@ public class Assignment10_ArrayLoops {
 		int[] arr = { 12, 34, 11, 36, 87, 98, 93 };
 		int i = 0;
 		int j;
-		
-		int g=arr.length-1; // array length -1
-		
+
+		int g = arr.length - 1; // array length -1
+
 		// printing the largest number in the array
 		for (i = 0; i < g; i++) {
 			for (j = 0; j < i; j++) {
-				if (arr[j] > arr[j + 1] && arr[i] > arr[i+1]) {
+				if (arr[j] > arr[j + 1] && arr[i] > arr[i + 1]) {
 					System.out.println(arr[i]);
-					
-		
+
 				}
-				
 
 			}
 		}
-	}}
-
-
+	}
+}
